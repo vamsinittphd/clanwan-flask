231e8310296bf18cbc05619df2ad6deff1e809b7
M Vamsi kiran <77319828+vamsinittphd@users.noreply.github.com>
2025-06-22 18:10:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.before_request
+def redirect_www():
+    if request.host.startswith("www."):
+        new_url = request.url.replace("://www.", "://", 1)
+        return redirect(new_url, code=301)
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
